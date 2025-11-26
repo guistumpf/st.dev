485c53ea827fc445c88283dd27c4703d1fcc5b13
João Guilherme Stumpf Fernandes <guist4@proton.me>
2025-11-25 23:45:57 -0300

    Add styling for texto2 and update hero section with additional information

--- app/page.tsx
+++ app/page.tsx
@@ -36,25 +36,30 @@ export default function App() {
         <h1>Suporte</h1>
       </nav>
 
-     <section>
-      <h1 className="head">Meus Sites</h1>
-     </section>
-<div className="hero">
-      <img src="/image1.png" alt="something" className="kanye"/>
-  <div className="hero-text">
-    <h1 className="subtitulo">Bem-Vindo!</h1>
-    <p className="texto">Olá! Meu nome é João e eu venho estudando desenvolvimento de sistemas
-      a algum tempo e recentemente eu tive a ideia de criar esse site para armazenar meus projetos e ideias.
-     
-     
-    </p>
-    <p className="texto">
-Sinta-se livre para testar e acompanhar minha jornada conforme meus conhecimentos vão se expandindo na área! 
-     </p>
-  </div>
-</div>
+      <section>
+        <h1 className="head">Meus Sites</h1>
+      </section>
+      <div className="hero">
+        <img src="/image1.png" alt="something" className="kanye" />
+        <div className="hero-text">
+          <h1 className="subtitulo">Bem-Vindo!</h1>
+          <p className="texto">Olá! Meu nome é João e eu venho estudando desenvolvimento de sistemas
+            a algum tempo e recentemente eu tive a ideia de criar esse site para armazenar meus projetos e ideias.
 
-    
+          </p>
+          <p className="texto">
+            Sinta-se livre para testar e acompanhar minha jornada conforme meus conhecimentos vão se expandindo na área!
+          </p>
+          <span></span>
+          <span></span>
+          <span></span>
+          <p className="texto2">
+            Clique na barrinha no canto superior esquerdo da página e veja meus projetos!
+          </p>
+        </div>
+      </div>
+
+
     </>
   )
 
