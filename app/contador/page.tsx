c53474ffd62f5dd5042f4b5bbc5cff5e139b38c3
João Guilherme Stumpf Fernandes <guist4@proton.me>
2025-11-27 21:36:55 -0300

    a pagina do contador ta completamente estilizada

--- app/contador/page.tsx
+++ app/contador/page.tsx
@@ -1,34 +1,52 @@
 "use client"
-
+import { useRouter } from "next/navigation"
+import "./index3.css"
 import { use, useState } from "react"
 
-export default function Contador(){
+export default function Contador() {
 
-const [numero, setnumero] = useState<number>(0)
-
-
-function add(){
-    setnumero(numero + 1)
-}
-
-function minus(){
-    setnumero(numero - 1)
-}
-
-function reset(){
-    setnumero(0)
-}
+    const [numero, setnumero] = useState<number>(0)
+const router = useRouter();
 
 
-return(
-    <section>
-        <h1>Contador</h1>
-        <p>{numero}</p>
-        <button onClick={add}>mais</button>
-        <button onClick={reset}>reset</button>
-        <button onClick={minus}>menos</button>
-    </section>
-)
+    let cor
+
+    function add() {
+        setnumero(numero + 1)
+    }
+
+    function minus() {
+        setnumero(numero - 1)
+    }
+
+    function reset() {
+        setnumero(0)
+    }
+
+    if (numero > 0) {
+        cor = "green"
+    } else if (numero < 0) {
+        cor = "red"
+    }
+
+
+
+    return (
+        <>
+
+            <div className="corpo2">
+
+                <section className="section">
+                    <h1 className="titulo">Contador</h1>
+                    <p style={{ color: cor }} className="numero">{numero}</p>
+                    <button onClick={add} className="porrinha">➕</button>
+                    <button onClick={reset} className="porrinha">🔃</button>
+                    <button onClick={minus} className="porrinha">➖</button>
+                </section>
+   <img src="klipartz.com.png" alt="voltar" className='back1' onClick={() => router.back()}/>
+            </div>
+        </>
+    )
 
 
 }
