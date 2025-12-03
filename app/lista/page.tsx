4312ff6ce6dc36bfb3e19892c49aab7aad6323a0
João Guilherme Stumpf Fernandes <guist4@proton.me>
2025-12-03 15:47:14 -0300

    adicionei o código fonte da lista

--- app/lista/page.tsx
+++ app/lista/page.tsx
@@ -53,7 +53,7 @@ export default function oi() {
 
           
          <img src="klipartz.com.png" alt="voltar" className='back' onClick={() => router.back()}/> 
-
+<a href="https://github.com/guistumpf/lista-next" className='fonte'>Código Fonte</a>
         <section className='Container'>
           <h1>Lista De Tarefas</h1>
           <input value={input} onChange={(e) => {
