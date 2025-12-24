48f3683504c3aac31f6490cc27512ad00098d657
Joao Fernandes <guist4@proton.me>
2025-12-24 20:07:51 -0300

    tudo certo, implementando um timeout aqui

--- app/dado/page.tsx
+++ app/dado/page.tsx
@@ -1,25 +1,37 @@
 'use client'
 
 import { use, useState } from "react"
+import "./index6.css"
 
 export default function Dados(){
 const [result, setresult] = useState("🎲")
+const [text, settext] = useState("Clique no botão para jogar!")
+ const [bloqueado, setblock] = useState(false) 
 const dados =  ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
 
     function random (){
         
     const randoma = Math.floor(Math.random() * 6)
     const resultado = dados[randoma]
-        setresult(resultado)
-
+        
+    settext("Você tirou " + randoma + "!")
+    setresult(resultado)
     console.log(resultado)
 }
 
 
 return(
-    <> 
-  <h1>{result}</h1>
-    <button onClick={random}>oioi</button>
+   <>
+<div className="dadocorpo">
+
+ <div className="dadodiv">
+    <h1 className="dadotitulo">Jogue o Dado!</h1>
+  <h1 className="dadosra">{result}</h1>
+    <button onClick={random} className="dadobut">Jogar o Dado</button>
+    <p className="pdado">{text}</p>
+    </div> 
+</div>
+   
     </>
 )
 
